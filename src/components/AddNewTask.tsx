16586d1b8ceae8d791f2e37095e6dea38cc345b5
João <joaovtrmorais0@gmail.com>
2024-12-28 15:41:23 -0300

    feat(components): add initial logic for AddNewTask component

--- src/components/AddNewTask.tsx
+++ src/components/AddNewTask.tsx
@@ -1,16 +1,40 @@
 import { PlusCircle } from '@phosphor-icons/react';
 
 import styles from './AddNewTask.module.css';
+import { ChangeEvent, InvalidEvent, useState } from 'react';
 
 
 export function AddNewTask() {
+    
+    const [newTaskText, setNewTaskText] = useState('')
+
+    function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+        event.target.setCustomValidity('');
+        setNewTaskText(event.target.value);
+    }
+
+    function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+        event.target.setCustomValidity('Esse campo é obrigatório.');
+    }
+
+    const isNewTaskEmpty = newTaskText.length == 0;
+
+
     return (
-        <div className={styles.container}>
-            <input type="text" placeholder='Adicione uma nova tarefa'/>
-            <button>
+        <form className={styles.container}>
+            <input 
+                name='task'
+                type="text" 
+                placeholder='Adicione uma nova tarefa'
+                value={newTaskText}
+                onChange={handleNewTaskChange}
+                onInvalid={handleNewTaskInvalid}
+                required
+            />
+            <button type='submit' disabled={isNewTaskEmpty}>
                 Criar
                 <PlusCircle size={22} color='#F2F2F2'/>
             </button>
-        </div>
+        </form>
     )
 }
